88c89d82e8a7c2b98346a6ae047bb4412e2db6b2
Lulu Cole <lilikoicole@gmail.com>
2025-08-06 21:06:00 -1000

    Add scaffolding for "home" and "new entry" options. includes basic date, on period, mood, and journal code within new entry adding

--- app.py
+++ app.py
@@ -5,3 +5,22 @@ st.title("Period Pal")
 menu = ["Home", "New Entry"]
 choice = st.sidebar.selectbox("Menu", menu)
 
+if choice == "Home":
+    st.header("Welcome to Period Pal")
+    df = load_entries()
+    if df.empty:
+        st.warning("No entries found. Add an entry in 'New Entry'")
+    else:
+        st.subheader("will add stuff here")
+elif choice == "New Entry":
+    st.header("Add a New Entry")
+
+    entry_date = st.date_input("Select Date", date.today())
+    on_period = st.checkbox("Period Today?")
+    mood = st.radio("How do you feel?", ['😁', '🙂', '😐','😔', '😖'])
+    journal = st.text_area("Journal Entry", max_chars=500)
+    if st.button("Save Entry"):
+        sentiment = analyze_sentiment(journal)
+        save_entry(entry_date, on_period, mood, journal)
+
+
